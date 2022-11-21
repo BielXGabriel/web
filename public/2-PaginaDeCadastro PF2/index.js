d65c348842d6968debaad5b9e31509de1de1ff3a
GABRIEL FERREIRA DOS SANTOS <E0512583@famacad.com.br>
2022-11-21 09:57:56 -0300

    mudei muita coisa boa sorte html e css

---
+++ public/2-PaginaDeCadastro PF2/index.js
@@ -0,0 +1,56 @@
+import './PaginaDeCadastro2.css'
+const PaginaDeCadastro2 = () =>{
+    return(
+        <div className='pagina'>
+            <div className='p-coluna'>
+                <div>
+                    <h1>Impulsione</h1>
+                    <h8>by Crefisa</h8>
+                    <h9>explorar -></h9>
+                </div>
+
+                <div>
+                    <h2>Desenvolver pessoas <br/>Impulsionar negócios <br/>Conectar pessoas</h2>
+                </div>
+
+                <div className='botoes'>
+                    <button className='bt1'><nobr>ENTRAR NA MINHA CONTA</nobr></button>
+                    <button className='bt2'><nobr>ME CADASTRAR COMO PF</nobr></button>
+                    <button className='bt3'><nobr>CADASTRAR MEU NEGÓCIO</nobr></button>
+                </div>
+
+            </div>
+
+
+            <div className='s-coluna'>
+                <div >
+                <h3><nobr> PESSOAL   </nobr>  </h3> 
+                <h4><nobr> ENDEREÇO  </nobr>  </h4> 
+                <h5><nobr> CADASTRO   </nobr>  </h5>
+                <h6><nobr> > </nobr></h6>
+                <h7><nobr> > </nobr></h7>
+
+
+                <div className='s-nome '>
+                  
+                
+                  
+                  
+                
+                </div>
+
+            </div>
+
+            <div className='s-coluna'></div>
+
+
+
+            </div>
+
+
+        </div>
+     
+
+    ) 
+}
+export default PaginaDeCadastro2;
